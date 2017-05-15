759f583a71183d404e2a08f844bbe68d7fe1615a
minto <minto@hotmail.fr>
2017-05-15 22:08:16 +0200

    reduce

--- golf_GISS.py
+++ golf_GISS.py
@@ -3,5 +3,4 @@ for o in __import__('shapefile').Reader(f[0]).shapes():
  for l in __import__('shapely.geometry',(),(),['']).shape(o):
   l=str(l)[10:-2].replace(', ',',').replace(' ',',').split(',');p=lambda c:__import__('pyproj').Proj(init='epsg:3395')(*c)
   c.create_polygon(sum(((p(c)[0],-p(c)[1])for c in zip(l[0::2],l[1::2])),()))
-c.bind('<MouseWheel>',lambda e:c.scale('all',e.x,e.y,(e.delta>0)*2or 0.5,(e.delta>0)*2or 0.5));w.title('pyGISS in 6L');mainloop()
-
+c.bind('<MouseWheel>',lambda e:c.scale('all',e.x,e.y,(e.delta>0)*2or 0.5,(e.delta>0)*2or 0.5));w.title('pyGISS in 6 lines');mainloop()

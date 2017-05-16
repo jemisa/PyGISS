61ad47a85557ba0a6d078833c6a9914e6b74f594
minto <minto@hotmail.fr>
2017-05-16 15:31:50 +0200

    reduce

--- golf_GISS.py
+++ golf_GISS.py
@@ -1,5 +1,5 @@
-from tkinter import*;from tkinter.filedialog import*;c=Canvas();c.pack();f=askopenfilenames()
+import tkinter as t, pyproj as p, tkinter.filedialog as d;c=t.Canvas();f=d.askopenfilenames();y=lambda g:p.Proj(init='epsg:3395')(*g)
 for o in __import__('shapefile').Reader(f[0]).shapes():
  for l in map(lambda s:str(s)[10:-2].replace(', ',',').replace(' ',',').split(','),__import__('shapely.geometry',(),(),['']).shape(o)):
-  p=lambda g:__import__('pyproj').Proj(init='epsg:3395')(*g);c.create_polygon(sum(((p(x)[0],-p(x)[1])for x in zip(l[0::2],l[1::2])),()))
-c.bind('<MouseWheel>',lambda e:c.scale('all',e.x,e.y,(e.delta>0)*2or 0.5,(e.delta>0)*2or 0.5));mainloop()
+  c.create_polygon(sum(((y(x)[0],-y(x)[1])for x in zip(l[0::2],l[1::2])),()))
+c.bind('<MouseWheel>',lambda e:c.scale('all',e.x,e.y,(e.delta>0)*2or 0.5,(e.delta>0)*2or 0.5));c.pack();t.mainloop()
